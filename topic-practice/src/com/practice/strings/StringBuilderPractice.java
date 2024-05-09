0740b958e404e9d4656fdd46bc43449cff55faa9
Ian Perfitt <iancp@protonnmail.com>
2024-05-09 16:36:37 -0500

    added target/ to .gitignore and practice with string + stringbuilder equality

---
+++ topic-practice/src/com/practice/strings/StringBuilderPractice.java
@@ -0,0 +1,13 @@
+package com.practice.strings;
+
+public class StringBuilderPractice {
+
+	public static void main(String[] args) {
+
+		String s1 = "hello";
+		StringBuilder s2 = new StringBuilder("hello");
+
+		System.out.println(s1 == s2);
+
+	}
+}
