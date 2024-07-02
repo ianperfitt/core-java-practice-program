b7cf59f10620f9d712261ef709511886c14ecf01
Ian Perfitt <iancp@protonnmail.com>
2024-07-02 16:24:50 -0500

    Created class for practicing concepts related to Multi-threading.

---
+++ topic-practice/src/Practice/ThreadPractice.java
@@ -0,0 +1,92 @@
+package Practice;
+
+import java.util.stream.IntStream;
+
+public class ThreadPractice {
+
+	// Inner class
+	class SharedResource {
+		private int data = 0;
+		private boolean available = false;
+
+		public void produce(int value) throws InterruptedException {
+			// Using a synchronized block of code for more granular control
+			synchronized (this) {
+				while (available) {
+					// Used for inter thread communication and
+					// synchronize the activities of multiple threads.
+					wait();
+				}
+				data = value;
+				available = true;
+				notify();
+			}
+		}
+
+		public int consume() throws InterruptedException {
+			synchronized (this) {
+				while (!available) {
+					wait();
+				}
+				available = false;
+				notify();
+				return data;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ThreadPractice threadPractice = new ThreadPractice();
+
+		SharedResource sharedResource = threadPractice.new SharedResource();
+
+		// Runnable passes method containing IntStream to 
+		// run() method
+		Runnable producerRunnable = () -> {
+
+			IntStream.range(1, 6).forEach((i) -> {
+				try {
+					sharedResource.produce(i);
+					System.out.println("Produced: " + i);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			});
+		};
+
+		Runnable consumerRunnable = () -> {
+
+			IntStream.range(1, 6).forEach((i) -> {
+				try {
+					int value = sharedResource.consume();
+					System.out.println("Consumed: " + value);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			});
+		};
+
+		// Producer and consumer in New state.
+		Thread producer = new Thread(producerRunnable);
+		Thread consumer = new Thread(consumerRunnable);
+
+		// Producer and consumer in Runnable state.
+		producer.start();
+		consumer.start();
+
+		// Ensure that both consumer and producer
+		// tasks have terminated before
+		// proceeding with main thread
+		producer.join();
+		consumer.join();
+
+		// At this point both producer
+		// and consumer are in terminated state
+		System.out.println("Producer is alive: " + producer.isAlive());
+		System.out.println("Consumer is alive: " + consumer.isAlive());
+
+		System.out.println("Final data count: " + sharedResource.data);
+	}
+}
