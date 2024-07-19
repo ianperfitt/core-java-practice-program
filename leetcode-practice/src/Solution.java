a525ff3ef7df3d80e01240906a0d37c237ad34da
Ian Perfitt <iancp@protonnmail.com>
2024-07-19 12:43:21 -0500

    Solved Leetcode problem 557. Reverse Words in a String III using Two Pointer Method in O(n) time.

--- leetcode-practice/src/Solution.java
+++ leetcode-practice/src/Solution.java
@@ -1,15 +1,27 @@
 class Solution {
 
 	public static String reverseWords(String s) {
+	     char[] stringArray = s.toCharArray();
+	     int left = 0;
+	     int right = 0;
+	     char temp = 'a';
 
-		String[] splitString = s.split(" ");
-		StringBuilder solution = new StringBuilder();
-		
-		for(String str : splitString) {
-			StringBuilder reverse = new StringBuilder(str).reverse().append(" ");
-			solution.append(reverse);
-		}
-		
-		return solution.toString().trim();
-	}
+	     for(int i = 0; i < stringArray.length; i++) {
+	        if(i + 1 == stringArray.length || stringArray[i + 1] == ' ') {
+	            // set right pointer without losing track of loop index
+	            right = i;
+	            while(left < right) {
+	                // swap all characters in word
+	                temp = stringArray[left];
+	                stringArray[left] = stringArray[right];
+	                stringArray[right] = temp;
+	                ++left;
+	                --right;
+	            }
+	            // position new left one past each space
+	            left = i + 2;
+	            }
+	        }
+	        return new String(stringArray);   
+	    }
 }
